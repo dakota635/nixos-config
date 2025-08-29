cb172c80604d239430883f0fd9ad85d9f364e1e7
Dakota Wilburn <dktwlbrn@gmail.com>
2025-08-29 16:30:12 -0500

    Add Waybar Cava module

---
+++ modules/home/dakota/applications/waybar/waybar-cava.nix
@@ -0,0 +1,12 @@
+{ ... }:
+
+{
+  framerate = 30;
+  autosens = 1;
+  bars = 14;
+  method = "pipewire";
+  bar_delimiter = 0;
+  input_delay = 4;
+  format-icons = [ "▁" "▂" "▃" "▄" "▅" "▆" "▇" "█" ];
+  actions = { "on-click-right" = "mode"; };
+}
--- modules/home/dakota/applications/waybar/waybar.nix
+++ modules/home/dakota/applications/waybar/waybar.nix
@@ -1,43 +1,54 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
   programs.waybar = {
     enable = true;
-    package = pkgs.waybar; # or pkgs.waybar-hyprland if available
+    package = pkgs.waybar;
     settings = {
-      mainBar = {
-        layer = "top";
-        position = "top";
-        height = 30;
-        margin = "6 0 0 0";
-        modules-left   = [ "hyprland/workspaces" "hyprland/window" ];
-        modules-center = [ "clock" ];
-        modules-right  = [
-          "tray" "pulseaudio" "network"
-          "bluetooth" "temperature" "cpu"
-          "memory" "battery" "backlight"
-          "idle_inhibitor"
-        ];
-        # Examples:
-        "clock" = { format = "{:%a %b %d  %H:%M}"; tooltip-format = "{:%F %T}"; };
-        "pulseaudio" = { format = "{icon} {volume}%"; format-muted = "󰖁"; };
-        "network" = { format-wifi = "󰤨 {signalStrength}%"; format-ethernet = "󰈁"; format-disconnected = "󰤮"; tooltip = true; };
-        "bluetooth" = { format = ""; format-connected = " {device_alias}"; };
-        "battery" = { format = "{icon} {capacity}%"; states = { critical = 15; warning = 30; }; };
-        "backlight" = { format = "󰃝 {percent}%"; };
-        "cpu" = { format = " {usage}%"; tooltip = false; };
-        "memory" = { format = " {used:0.0f}G"; tooltip = false; };
-        "temperature" = { format = " {temperatureC}°C"; hwmon-path = "/sys/class/thermal/thermal_zone0/temp"; };
-        "idle_inhibitor" = { format = "{icon}"; format-icons = { activated = ""; deactivated = ""; }; };
-        "hyprland/window" = { format = "{}"; max-length = 80; };
-        "hyprland/workspaces" = {
-          format = "{icon}";
-          on-click = "activate";
-          format-icons = { "1"=""; "2"=""; "3"=""; "4"="󰓇"; "5"="󰈙"; };
-          persistent-workspaces = { "*" = [ 1 2 3 4 5 ]; };
-        };
-        "tray" = { spacing = 8; };
-      };
+      mainBar = lib.mkMerge [
+        { height = lib.mkDefault 36; }
+        {
+          layer = "top";
+          position = "top";
+          height = 30;
+          margin = "6 0 0 0";
+          modules-left   = [ "hyprland/workspaces" "hyprland/window" ];
+          modules-center = [ "clock" ];
+          modules-right  = [
+            "cava" 
+            "tray"
+            "pulseaudio"
+            "network"
+            "bluetooth"
+            "temperature"
+            "cpu"
+            "memory"
+            "battery"
+            "backlight"
+            "idle_inhibitor"
+          ];
+
+          "clock" = { format = "{:%a %b %d  %H:%M}"; tooltip-format = "{:%F %T}"; };
+          "pulseaudio" = { format = "{icon} {volume}%"; format-muted = "󰖁"; };
+          "network" = { format-wifi = "󰤨 {signalStrength}%"; format-ethernet = "󰈁"; format-disconnected = "󰤮"; tooltip = true; };
+          "bluetooth" = { format = ""; format-connected = " {device_alias}"; };
+          "battery" = { format = "{icon} {capacity}%"; states = { critical = 15; warning = 30; }; };
+          "backlight" = { format = "󰃝 {percent}%"; };
+          "cpu" = { format = " {usage}%"; tooltip = false; };
+          "memory" = { format = " {used:0.0f}G"; tooltip = false; };
+          "temperature" = { format = " {temperatureC}°C"; hwmon-path = "/sys/class/thermal/thermal_zone0/temp"; };
+          "idle_inhibitor" = { format = "{icon}"; format-icons = { activated = ""; deactivated = ""; }; };
+          "hyprland/window" = { format = "{}"; max-length = 80; };
+          "hyprland/workspaces" = {
+            format = "{icon}";
+            on-click = "activate";
+            format-icons = { "1"=""; "2"=""; "3"=""; "4"="󰓇"; "5"="󰈙"; };
+            persistent-workspaces = { "*" = [ 1 2 3 4 5 ]; };
+          };
+          "cava" = import ./waybar-cava.nix { };
+          "tray" = { spacing = 8; };
+        }
+      ];
     };
   };
 }
