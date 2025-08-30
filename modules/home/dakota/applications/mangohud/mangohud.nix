1d8723373f1226e5aa56b7434fb8bf6511cb72df
Dakota Wilburn <dktwlbrn@gmail.com>
2025-08-29 20:06:43 -0500

    Update MangoHud HUD

--- modules/home/dakota/applications/mangohud/mangohud.nix
+++ modules/home/dakota/applications/mangohud/mangohud.nix
@@ -6,16 +6,41 @@
     enableSessionWide = true;
     
     settings = {
-      cpu_stats = true;
+      alpha = lib.mkForce 1.0;
+      background_alpha = lib.mkForce 0.0;
+
+      # GPU data
+      gpu_text = "GPU";
       gpu_stats = true;
-      ram = true;
       vram = true;
+      ram = true;
+      gpu_core_clock = true;
+      gpu_mem_clock = true;
+      gpu_temp = true;
+
+      # CPU data
+      cpu_text = "CPU";
+      cpu_stats = true;
+      core_load = true;
+      core_bars = true;
+      cpu_mhz = true;
+      cpu_temp = true;
+
+      # Game data
       fps = true;
-      frametime = true;
-      frametime_graph = true;
-      throttling_status = true;
-      throttling_status_graph = true;
-      background_alpha = lib.mkForce 0.5;
+      engine_version = true;
+
+      # Misc data
+      engine_short_names = true;
+      vulkan_driver = true;
+      frame_timing = true;
+
+      # Font settings
+      no_small_font = true;
+
+      # HUD settings
+      hud_no_margin = true;
+      table_columns = 4;
     };
   };
 }
