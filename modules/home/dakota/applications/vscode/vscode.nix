55236bfd0a440f5e66ae93be9889c9671047a1f0
Dakota Wilburn <dktwlbrn@gmail.com>
2025-08-29 22:12:03 -0500

    Reorder waybar and update icons

--- modules/home/dakota/applications/vscode/vscode.nix
+++ modules/home/dakota/applications/vscode/vscode.nix
@@ -1,8 +1,14 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
   programs.vscode = {
     enable = true;
+    
+    userSettings = {
+      "editor.fontFamily" = lib.mkForce "JetBrainsMono Nerd Font, Symbols Nerd Font, monospace";
+      "editor.fontLigatures" = true;
+    };
+
     profiles.default.extensions = with pkgs.vscode-extensions; [
       bbenoist.nix
     ];
--- modules/home/dakota/applications/waybar/waybar.nix
+++ modules/home/dakota/applications/waybar/waybar.nix
@@ -5,50 +5,65 @@
     enable = true;
     package = pkgs.waybar;
     settings = {
-      mainBar = lib.mkMerge [
-        { height = lib.mkDefault 36; }
-        {
-          layer = "top";
-          position = "top";
-          height = 30;
-          margin = "6 0 0 0";
-          modules-left   = [ "hyprland/workspaces" "hyprland/window" ];
-          modules-center = [ "clock" ];
-          modules-right  = [
-            "cava" 
-            "tray"
-            "pulseaudio"
-            "network"
-            "bluetooth"
-            "temperature"
-            "cpu"
-            "memory"
-            "battery"
-            "backlight"
-            "idle_inhibitor"
-          ];
+      mainBar = {
+        layer = "top";
+        position = "top";
+        height = 36;
+        margin = "6 0 0 0";
+        modules-left   = [ "hyprland/workspaces" "hyprland/window" ];
+        modules-center = [ "clock" ];
+        modules-right  = [
+          "cava" 
+          "wireplumber"
+          "network"
+          "temperature"
+          "cpu"
+          "memory"
+          "battery"
+          "bluetooth"
+          "idle_inhibitor"
+        ];
 
-          "clock" = { format = "{:%a %b %d  %H:%M}"; tooltip-format = "{:%F %T}"; };
-          "pulseaudio" = { format = "{icon} {volume}%"; format-muted = "󰖁"; };
-          "network" = { format-wifi = "󰤨 {signalStrength}%"; format-ethernet = "󰈁"; format-disconnected = "󰤮"; tooltip = true; };
-          "bluetooth" = { format = ""; format-connected = " {device_alias}"; };
-          "battery" = { format = "{icon} {capacity}%"; states = { critical = 15; warning = 30; }; };
-          "backlight" = { format = "󰃝 {percent}%"; };
-          "cpu" = { format = " {usage}%"; tooltip = false; };
-          "memory" = { format = " {used:0.0f}G"; tooltip = false; };
-          "temperature" = { format = " {temperatureC}°C"; hwmon-path = "/sys/class/thermal/thermal_zone0/temp"; };
-          "idle_inhibitor" = { format = "{icon}"; format-icons = { activated = ""; deactivated = ""; }; };
-          "hyprland/window" = { format = "{}"; max-length = 80; };
-          "hyprland/workspaces" = {
-            format = "{icon}";
-            on-click = "activate";
-            format-icons = { "1"=""; "2"=""; "3"=""; "4"="󰓇"; "5"="󰈙"; };
-            persistent-workspaces = { "*" = [ 1 2 3 4 5 ]; };
-          };
-          "cava" = import ./waybar-cava.nix { };
-          "tray" = { spacing = 8; };
-        }
-      ];
+        "clock" = { format = "{:%a %b %d  %H:%M}"; tooltip-format = "{:%F %T}"; };
+        "wireplumber" = {
+          format = "   {volume}%";
+          format-muted = " 󰖁";
+          on-click = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+          on-scroll-up = "wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+";
+          on-scroll-down = "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-";
+        };
+        "network" = { format-wifi = "󰤨  {signalStrength}%"; format-ethernet = "󰈁"; format-disconnected = "󰤮"; tooltip = true; };
+        "bluetooth" = { format = ""; format-connected = " {device_alias}"; };
+        "battery" = {
+          format = "{icon} {capacity}%";
+          states = { critical = 15; warning = 30; };
+          format-icons = [
+            "󰂎" # 0–10%
+            "󰁺" # 10–20%
+            "󰁻" # 20–30%
+            "󰁼" # 30–40%
+            "󰁽" # 40–50%
+            "󰁾" # 50–60%
+            "󰁿" # 60–70%
+            "󰂀" # 70–80%
+            "󰂁" # 80–90%
+            "󰂂" # 90–100%
+            "󰁹" # charging
+          ];
+        };
+        "cpu" = { format = "  {usage}%"; tooltip = false; };
+        "memory" = { format = "  {used:0.0f}G"; tooltip = false; };
+        "temperature" = { format = " {temperatureC}°C"; hwmon-path = "/sys/class/thermal/thermal_zone0/temp"; };
+        "idle_inhibitor" = {format = "{icon}"; format-icons = { activated = " "; deactivated = " "; }; };
+        "hyprland/window" = { format = "{}"; max-length = 80; };
+        "hyprland/workspaces" = {
+          format = "{icon}";
+          on-click = "activate";
+          format-icons = { "1"=""; "2"=""; "3"=""; "4"="󰓇"; "5"="󰈙"; };
+          persistent-workspaces = { "*" = [ 1 2 3 4 5 ]; };
+        };
+        "cava" = import ./waybar-cava.nix { };
+      };
     };
   };
 }
