2467fd4282ee8e8999d90280f6ddc216c18344a2
dakota635 <dktwlbrn@gmail.com>
2025-09-08 19:36:17 -0500

    Remove flake-parts and simplify main flake

--- flake.nix
+++ flake.nix
@@ -2,12 +2,7 @@
   description = "NixOS configuration";
 
   inputs = {
-    devshell = {
-      url = "github:numtide/devshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -18,8 +13,6 @@
       url = "github:hyprwm/hyprland";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     rose-pine-hyprcursor = {
       url = "github:ndom91/rose-pine-hyprcursor";
@@ -33,21 +26,28 @@
     };
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
-      imports = [
-        # NixOS Hosts
-        # ./modules/flake-parts/hosts/mermaid-man/flake.nix
-        ./modules/flake-parts/hosts/barnacle-boy/flake.nix
-        ./modules/flake-parts/hosts/dirty-bubble/flake.nix
-
-        # Devshell
-        ./modules/flake-parts/devshell/base/java/java.nix
-        ./modules/flake-parts/devshell/base/java/java-packages.nix
-
-        ./modules/flake-parts/devshell/base/python/python.nix
-        ./modules/flake-parts/devshell/base/python/python-packages.nix
-      ];
+  outputs = inputs@{ self, nixpkgs, home-manager, stylix, ... }:
+  let
+    mkHost = { machinePath }:
+      nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./modules/nixos
+          home-manager.nixosModules.home-manager
+          stylix.nixosModules.stylix
+          machinePath
+          {
+            home-manager.users.dakota.imports = [
+              ./modules/home/dakota
+            ];
+          }
+        ];
+      };
+  in {
+    nixosConfigurations = {
+      barnacle-boy = mkHost { machinePath = ./modules/hosts/barnacle-boy; };
+      dirty-bubble  = mkHost { machinePath = ./modules/hosts/dirty-bubble; };
     };
-  }
+  };
+}
--- modules/home/dakota/default.nix
+++ modules/home/dakota/default.nix
@@ -2,6 +2,14 @@
 
 {
   imports = [
-    ./applications
+    ./audio
+    ./broswer
+    ./desktop-env
+    ./dev-tools
+    ./file-management
+    ./gaming
+    ./home-manager
+    ./shell
+    ./terminal
   ];
 }
